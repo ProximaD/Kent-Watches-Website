38fd517aecaaae6312ed0693f39b46eacb50036a
Sandra Lavenda <111272273+ProximaD@users.noreply.github.com>
2024-09-20 15:16:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-/*=============== SHOW MENU ===============*/
+/*=============== MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
 
-/* Menu Show */
+/* Show Menu  */
 if(navToggle){
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
@@ -12,7 +12,7 @@ if(navToggle){
 }
 
 
-/* Menu Show */
+/* Hide Menu */
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
