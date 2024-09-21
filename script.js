4f8f2189a3a4aab109f43d8310fe32a096213e28
Sandra Lavenda <111272273+ProximaD@users.noreply.github.com>
2024-09-21 06:25:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -30,4 +30,33 @@ const linkAction = () =>{
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+/*=============== INITIALIZE SWIPER JS ===============*/
+const swiperWatches = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+    effect: 'creative',
+    creativeEffect: {
+        prev: {
+            translate: [-100, 0, -500],
+            rotate: [0, 0, 15],
+            opacity: 0
+        },
+        prev: {
+            translate: [100, 0, -500],
+            rotate: [0, 0, -15],
+            opacity: 0
+        },
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  })
+
+/*=============== GSAP ANIMATION ===============*/
+
+
+
 
